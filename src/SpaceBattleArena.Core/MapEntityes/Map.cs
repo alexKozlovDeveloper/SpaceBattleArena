b2f6f908582ff18fbe685dbe1cbc6a927fa47347
alexKozlovDeveloper <alexkozlovdeveloper@gmail.com>
2017-12-27 05:27:04 -0500

    Update structure

---
+++ src/SpaceBattleArena.Core/MapEntityes/Map.cs
@@ -0,0 +1,22 @@
+﻿using SpaceBattleArena.Core.PlayersEntityes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpaceBattleArena.Core.MapEntityes
+{
+    public class Map
+    {
+        public List<Player> Players;
+
+        public List<Bullet> Bullets;
+
+        public Map()
+        {
+            Players = new List<Player>();
+            Bullets = new List<Bullet>();
+        }
+    }
+}
---
+++ src/SpaceBattleArena.Web/Controllers/ArenaController.cs
@@ -0,0 +1,31 @@
+﻿using SpaceBattleArena.Core;
+using SpaceBattleArena.Core.MapEntityes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Helpers;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class ArenaController : Controller
+    {
+        // GET: Arena
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public JsonResult GetMapInfo()
+        {
+            var map = Session[Constants.SessionKeys.MapKey] as Map;
+            return Json(map);
+        }
+
+        public void UpdatePlayerState()
+        {
+
+        }
+    }
+}
