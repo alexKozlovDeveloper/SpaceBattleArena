6829d6ef24fb9bc3ed31b620abe99c228162daf6
alexKozlovDeveloper <alexkozlovdeveloper@gmail.com>
2017-12-27 06:01:23 -0500

    add typeScript

--- src/SpaceBattleArena.Web/App_Start/BundleConfig.cs
+++ src/SpaceBattleArena.Web/App_Start/BundleConfig.cs
@@ -26,6 +26,9 @@ namespace SpaceBattleArena.Web
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+
+            bundles.Add(new ScriptBundle("~/bundles/tsScripts").Include(
+                      "~/appScriptsJS/app.js"));
         }
     }
 }
